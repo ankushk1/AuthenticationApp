b92cd52c1941cac9ab382027c94100584fe0d4b4
ankushk1 <40850518+ankushk1@users.noreply.github.com>
2020-04-17 20:17:25 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// // const bcrypt = require('bcryptjs');
-// const passport = require('passport');
-// // // Load User model
-// // const User = require('../models/User');
-
-//
-// const {
-//   forwardAuthenticated
-// } = require('../config/auth');
-
-// // Login Page
-// router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
-
-// // Register Page
-// router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
-
-// // Register
-// router.post('/register', userController.register);
-
-// // Login
-// router.post('/login', userController.login);
-
-// // Logout
-// router.get('/logout', (req, res, next) => {
-//   passport.authenticate('local', {
-//     successRedirect: res.redirect('/dashboard'),
-//     failureRedirect: '/users/signin',
-//     failureFlash: true
-//   });
-// });
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const {
